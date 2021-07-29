016bddcb254df476d7ff1246388f716c4aa5b530
Abidur Rahman Akash <abidurrahman471@gmail.com>
2021-07-29 15:53:08 +0600

    component refactor

--- src/services/firebase/firebaseInit.js
+++ src/services/firebase/firebaseInit.js
@@ -7,15 +7,6 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBqOBQNTMzfPgf9mTLmykDnY_JvTKHUavI",
-  authDomain: "social-blog-app-7b23b.firebaseapp.com",
-  projectId: "social-blog-app-7b23b",
-  storageBucket: "social-blog-app-7b23b.appspot.com",
-  messagingSenderId: "806069075685",
-  appId: "1:806069075685:web:40a4a897a74a76b4a89e0c",
-};
-
 let app;
 
 if (firebase.apps.length === 0) {
